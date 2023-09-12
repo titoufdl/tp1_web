bd64427cab8e9e3f8fd1ad9eb4c4a7583f62c8c9
Ferdoel Titouan <o22203653@info03-25.iut45.univ-orleans.fr>
2023-09-12 14:51:47 +0200

    ajout de fonctionnalitées static

--- tuto/views.py
+++ tuto/views.py
@@ -1,5 +1,9 @@
 from .app import app
+from flask import render_template
 
 @app.route("/")
 def home():
-    return "<h1>Hello World</h1>"
+    return render_template(
+        "home.html",
+        title="Hello World!",
+        names=["Pierre", "Paul", "Corinne"])
