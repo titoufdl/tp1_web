ca374ae47b948c4f6e34b343fa43e738c7253532
Ferdoel Titouan <o22203653@info03-25.iut45.univ-orleans.fr>
2023-09-12 15:29:29 +0200

    ajout fin du cours

--- tuto/views.py
+++ tuto/views.py
@@ -4,6 +4,14 @@ from flask import render_template
 @app.route("/")
 def home():
     return render_template(
-        "home.html",
+        "base.html",
+        title="Hello World!")
+    
+@app.route("/noms")
+def noms():
+    return render_template(
+        "noms.html",
         title="Hello World!",
         names=["Pierre", "Paul", "Corinne"])
+    
+        
